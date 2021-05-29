7c120ea40a37f5a14688ad4ae3130380ceaf874a
deviswamy1987 <deviswamy1987@gmail.com>
2021-05-29 12:48:57 +0100

    created a get request and dynamic routes

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
-console.log("Hello server");
+//Include Express JS
+const express = require("express");
+
+//Let create a new express Instance like object
+const app = express();
+//create a GET REQUEST HANDLER
+
+app.get("/", (req, res) => {
+  res.json("Hello devi");
+});
+
+app.get("/:name", (req, res) => {
+  console.log(req.query);
+  res.json("Hey now the url is having the param  " + req.params.name + " !!");
+});
+
+//listen it in a particular port
+const port = 3000;
+app.listen(port, () => {
+  console.log("server started with port 3000");
+});
+
+module.exports = express;
